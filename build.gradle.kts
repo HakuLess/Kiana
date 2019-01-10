cc91399c882cd3f14f553d4449dc3c0d4937e015
马双飞 <mashuangfei@meituan.com>
2019-01-10 17:27:16 +0800

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        maven("https://maven.aliyun.com/repository/jcenter")
+        maven("https://maven.aliyun.com/repository/google")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        maven("https://maven.aliyun.com/repository/jcenter")
+        maven("https://maven.aliyun.com/repository/google")
+    }
+}
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
