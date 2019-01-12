a07f50970c0d02322f398bf59e44cfa818d91d72
马双飞 <mashuangfei@meituan.com>
2019-01-12 13:01:44 +0800

    add transform

--- app/src/main/java/com/haku/kiana/MainActivity.kt
+++ app/src/main/java/com/haku/kiana/MainActivity.kt
@@ -10,14 +10,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val a = true
+
+    val c = 300000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            Snackbar.make(view, "$c", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
